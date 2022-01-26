23ded2eb89ffc550fb9e7de7edd721866b6bca69
unknown <G00411347@gmit.ie>
2022-01-26 21:54:27 +0000

    week 02 - second commit

---
+++ Week02/extraQuestions.py
@@ -0,0 +1,17 @@
+# extraQuestions.py
+# Just solutions to extra questions
+# Author: Jamie Roche 
+
+
+
+print("21 - 4 = {}" .format(21-4))
+
+ans = 21 - 4
+print("21 - 4 = {}" .format(ans))
+
+num1 = int(input("Enter your 1st num:"))
+num2 = int(input("Enter your 2nd num:"))
+ans = num1 - num2 
+print("{} - {} = {}" .format(num1,num2,ans))
+
+print("Does 2 = 3? {}" .format(2==3))
---
+++ Week02/nameAndAge.py
@@ -0,0 +1,8 @@
+# nameAndAge.py
+# Just to print name and age 
+# Author: Jamie Roche 
+
+name = input("Enter your name please:")
+age = input("Enter your age please:")
+
+print("Hello {},\t your age is {}" .format(name,age) ) 
