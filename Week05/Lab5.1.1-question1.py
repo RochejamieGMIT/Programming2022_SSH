c421708efb8dec055fce6c92c5ce71875f20d291
unknown <G00411347@gmit.ie>
2022-03-03 15:06:40 +0000

    week 05 commit

---
+++ Week05/Lab5.1.1-question1.py
@@ -0,0 +1,41 @@
+numberOfQuestions = 5
+averageAge = 23.4
+debugMode = True
+name = "joe"
+ages = []
+months = ('Jan', 'Feb', 'Mar')
+book = {}
+stuff = [ 12 , 'Fred', False, {}]
+someone = dict(firstname = "joe")
+me = {
+"firstName" : "Andrew",
+"teaching" : [{
+"courseName" : "programming",
+"semester" : 1
+},{
+"courseName" : "Data Representation",
+"semester" : 2
+}
+]
+}
+
+print(me["teaching"][0]["courseName"])
+
+print(type(me["teaching"][0]["courseName"]))
+
+# A. int
+# B. Float
+# C. Boolean
+# D. String
+# E. List
+# F. Tuple
+# G. String
+# H. Dict
+# I. List
+# J. Boolean
+# K. Dict
+# L. String
+# M. Dict
+# N. List
+# O. Int
+# P. coursename is not a variable, courseName is a string
